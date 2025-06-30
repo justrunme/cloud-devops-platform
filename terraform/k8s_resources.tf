56b6457182ca7ba0e7f2e8e638690a21b806e8b7
justrunme <justrunme@gmail.com>
2025-06-30 18:16:56 +0200

    feat: Implement two-phase Terraform apply for Kind cluster and K8s resources.

--- terraform/k8s_resources.tf
+++ terraform/k8s_resources.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    kind = {
-      source  = "tehcyx/kind"
-      version = "0.0.13"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.20"
@@ -12,55 +8,16 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.9"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.4"
-    }
   }
-}
-
-provider "kind" {}
-
-resource "kind_cluster" "default" {
-  name           = "cloud-devops-platform"
-  wait_for_ready = true
-
-  kind_config {
-    kind        = "Cluster"
-    api_version = "kind.x-k8s.io/v1alpha4"
-
-    node {
-      role = "control-plane"
-      extra_port_mappings {
-        container_port = 30000
-        host_port      = 80
-        protocol       = "TCP"
-      }
-      extra_port_mappings {
-        container_port = 30001
-        host_port      = 443
-        protocol       = "TCP"
-      }
-    }
-    node {
-      role = "worker"
-    }
-  }
-}
-
-resource "local_file" "kubeconfig" {
-  depends_on = [kind_cluster.default]
-  content    = kind_cluster.default.kubeconfig
-  filename   = "${path.module}/kubeconfig"
 }
 
 provider "kubernetes" {
-  config_path = local_file.kubeconfig.filename
+  config_path = "${path.module}/kubeconfig"
 }
 
 provider "helm" {
   kubernetes {
-    config_path = local_file.kubeconfig.filename
+    config_path = "${path.module}/kubeconfig"
   }
 }
 
